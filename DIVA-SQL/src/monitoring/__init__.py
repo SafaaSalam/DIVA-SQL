b13a42d20468f10113cf4da303be756c05a31abb
Safaa Salam <safaaalatabbi@hotmail.com>
2026-01-10 16:25:56 +0300

    Delete DIVA-SQL directory
    Delate Files

--- DIVA-SQL/src/monitoring/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Performance monitoring for DIVA-SQL
-"""
-
-from .performance_tracker import PerformanceTracker
-from .metrics_collector import MetricsCollector
-
-__all__ = ['PerformanceTracker', 'MetricsCollector']
--- DIVA-SQL/src/templates/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Template-based SQL generation system for DIVA-SQL
-"""
-
-from .template_library import TemplateLibrary, SQLTemplate
-from .template_selector import TemplateSelector
-
-__all__ = ['TemplateLibrary', 'SQLTemplate', 'TemplateSelector']
